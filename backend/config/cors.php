f3c7c23c12cd4db53b751028d82497a61e5849c0
zhangyuhao018 <zhangyuhao018@gmail.com>
2026-02-10 19:32:56 +0900

    update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,18 +1,29 @@
 <?php
 
+$parseCsv = static function (string $value): array {
+    return array_values(array_filter(array_map('trim', explode(',', $value))));
+};
+
+$defaultOrigins = [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:3000',
+    'http://localhost.sekouAI.com',
+];
+
+$envOrigins = $parseCsv((string) env('CORS_ALLOWED_ORIGINS', ''));
+$envOriginPatterns = $parseCsv((string) env('CORS_ALLOWED_ORIGINS_PATTERNS', ''));
+$frontendUrl = trim((string) env('FRONTEND_URL', ''));
+$frontendOrigins = $frontendUrl !== '' ? [$frontendUrl] : [];
+
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'http://localhost:3000',
-        'http://localhost.sekouAI.com',
-    ],
+    'allowed_origins' => array_values(array_unique(array_merge($defaultOrigins, $frontendOrigins, $envOrigins))),
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => $envOriginPatterns,
 
     'allowed_headers' => ['*'],
 
